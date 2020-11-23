9c2370ea13d52315b178292fb631e9369d1a3c8e
Heltzel <heltzel.service@hotmail.com>
2020-11-23 12:21:58 +0100

    chapter 10: (checking) prop-types. done

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React from 'react'
+// notice the UPPER case
+import PropTypes from 'prop-types'
+import defaultImage from '../../../assets/default-image.jpeg'
 
-const Product = () => {
-  return <article className='product'>single product</article>;
-};
+const Product = ({ image, name, price }) => {
+  return (
+    <article className="product">
+      <img src={image.url} alt={name} />
+      <h4>{name}</h4>
+      <p>&euro; {price}</p>
+    </article>
+  )
+}
 
-export default Product;
+// the propTypes  PROPERTY of (in this case) Product is CAMEL case
+Product.propTypes = {
+  // here is used the import => all Uppercase
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+}
+
+// defautlProps is a React prop
+Product.defaultProps = {
+  name: 'default name',
+  image: { url: defaultImage },
+  price: 0.0,
+}
+export default Product
